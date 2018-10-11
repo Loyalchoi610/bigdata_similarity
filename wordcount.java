58bd571bd4111c6052d02d70c2fa2f73be532461
Choi Seungeon <chou610@naver.com>
2018-10-11 14:47:17 +0900

    bigdata similar non-friend pair part1 finished

--- wordcount.java
+++ wordcount.java
@@ -36,6 +36,7 @@ public class wordcount {
         // 0 | 1 2 3
         // 1 | 3 4 5
         JavaPairRDD<Integer,Integer> init = data.flatMapToPair(p -> {
+
             List<Tuple2<Integer,Integer>> result = new ArrayList<>();
             // Fill in here
             String[] tmp = p.split("\t");
@@ -90,14 +91,21 @@ public class wordcount {
                 }
 
             }
-            Collections.sort(keys);
+//            Collections.sort(keys);
+            int a,b, temp;
             int len = keys.size();
             if(len>1){
                 for(int i=0; i<len-1; i++){
                     for (int j=i+1; j<len; j++){
-//
-                        if(!friendMap.get(keys.get(i)).contains(keys.get(j))){
-                            result.add(new Tuple2<>(new Tuple2<>(keys.get(i),keys.get(j)),1));
+                        a = keys.get(i);
+                        b = keys.get(j);
+                        if(a>b){
+                            temp = a;
+                            a = b;
+                            b = temp;
+                        }
+                        if(!friendMap.get(a).contains(b)){
+                            result.add(new Tuple2<>(new Tuple2<>(a,b),1));
                         }
 
                     }
@@ -122,30 +130,28 @@ public class wordcount {
         for (Tuple2<Tuple2<Integer,Integer>,Integer> aa : wordCounts.collect()) {
 //            result.add(aa);
 //            caculate jaccard
-
-            if(aa._2()>THRESHOLD*(sizeInfo.get(aa._1()._1()) + sizeInfo.get(aa._1()._2()))/(1+THRESHOLD)){
+            if(((double)aa._2()/(sizeInfo.get(aa._1()._1()) + sizeInfo.get(aa._1()._2()) - aa._2())) >= THRESHOLD){
                 result.add(aa);
             }
 
         }
-        Collections.sort(result, new Comparator<Tuple2<Tuple2<Integer, Integer>, Integer>>() {
-            @Override
-            public int compare(Tuple2<Tuple2<Integer, Integer>, Integer> o1, Tuple2<Tuple2<Integer, Integer>, Integer> o2) {
-                int val = Integer.compare(o1._1()._1(),o2._1()._1());
-                if(val==-1){
-                    return -1;
-                }else if(val==0){
-                    return Integer.compare(o1._1()._2(), o2._1()._2());
-                }else{
-                    return 1;
-                }
-            }
-        });
+//        Collections.sort(result, new Comparator<Tuple2<Tuple2<Integer, Integer>, Integer>>() {
+//            @Override
+//            public int compare(Tuple2<Tuple2<Integer, Integer>, Integer> o1, Tuple2<Tuple2<Integer, Integer>, Integer> o2) {
+//                int val = Integer.compare(o1._1()._1(),o2._1()._1());
+//                if(val==-1){
+//                    return -1;
+//                }else if(val==0){
+//                    return Integer.compare(o1._1()._2(), o2._1()._2());
+//                }else{
+//                    return 1;
+//                }
+//            }
+//        });
         int len = result.size();
         for(int i=0; i<len; i++){
             Tuple2<Tuple2<Integer,Integer>,Integer> temp = result.get(i);
-            System.out.println(temp._1() + " " + temp._2() + " "
-                    + THRESHOLD*(sizeInfo.get(temp._1()._1()) + sizeInfo.get(temp._1()._2()))/(1+THRESHOLD));
+            System.out.println(temp._1() + " " + temp._2());
         }
         System.out.println("size : " + result.size());
 
